ba00c0085149baafe861b055ead31194cec5a051
Ehab-24 <ehabs1775@gmail.com>
2023-12-24 22:40:52 +0500

    second commit

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,60 @@
+extern crate rustc_serialize;
+extern crate sha1;
+
+use rustc_serialize::base64::{ToBase64, STANDARD};
+use sha1::{Digest, Sha1};
+
 use std::net::SocketAddr;
 use std::{collections::HashMap, error::Error};
 
-use std::io;
+use std::io::{self, Read};
 use std::time::Duration;
 
-use mio::net::TcpStream;
-use mio::{net::TcpListener, Events, Interest, Poll, Token};
+use mio::{net::TcpListener, net::TcpStream, Events, Interest, Poll, Token};
 
-pub struct Client {
+use http_muncher::{Parser, ParserHandler};
+
+struct HttpHandler;
+impl ParserHandler for HttpHandler {}
+
+pub struct WebSocketClient {
     socket: TcpStream,
     addr: SocketAddr,
+    http_parser: Parser,
 }
 
-impl Client {
+impl WebSocketClient {
     pub fn new(socket: TcpStream, addr: SocketAddr) -> Self {
-        Client { socket, addr }
+        WebSocketClient {
+            socket,
+            addr,
+            http_parser: Parser::request(),
+        }
+    }
+
+    pub fn read(&mut self) {
+        let mut buf = Vec::<u8>::new();
+        loop {
+            match self.socket.read_to_end(&mut buf) {
+                Ok(size) => {
+                    println!("read {} bytes", size);
+                    self.http_parser.parse(&mut HttpHandler {}, &buf[0..size]);
+                    if self.http_parser.is_upgrade() {
+                        println!("upgrading {}", self.addr);
+                        break;
+                    }
+                }
+                Err(_) => {
+                    println!("Error while reading socket");
+                    return;
+                }
+            }
+        }
     }
 }
 
 pub struct WebSocketServer {
-    clients: HashMap<Token, Client>,
+    clients: HashMap<Token, WebSocketClient>,
     socket: mio::net::TcpListener,
     token: Token,
     token_counter: usize,
@@ -53,17 +88,32 @@ impl WebSocketServer {
                             Ok((conn, addr)) => {
                                 println!("connection established with {}", addr);
 
-                                let client = Client::new(conn, addr);
+                                let client = WebSocketClient::new(conn, addr);
                                 self.clients.insert(Token(self.token_counter), client);
                                 self.token_counter += 1;
                             }
-                            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => break,
+                            Err(ref err) if would_block(err) => break,
                             Err(err) => return Err(Box::new(err)),
                         }
                     },
-                    Token(_) => unreachable!(),
+
+                    token => match self.clients.get_mut(&token) {
+                        Some(client) => {
+                            client.read();
+                            poller.registry().reregister(
+                                &mut client.socket,
+                                token,
+                                Interest::READABLE,
+                            )?;
+                        }
+                        None => println!("unknown token: {:?}", token),
+                    },
                 }
             }
         }
     }
+}
+
+fn would_block(err: &io::Error) -> bool {
+    err.kind() == io::ErrorKind::WouldBlock
 }
